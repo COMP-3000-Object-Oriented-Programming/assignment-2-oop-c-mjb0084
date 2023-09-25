925a137a1b3c4b7c95a76e2fd2817b692f4ae483
mjb0084 <mjb0084@auburn.edu>
2023-09-25 17:32:50 -0500

    Exercise 1 complete

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -6,18 +6,54 @@ using namespace std;
 int date, month, day, dayValue;
 int year = -1;
 
-int yearCheck(int year){
-    while (!((0 <= year) && (year <= 9999))){
-    cout << "Please enter a year between 0 and 9999AD: ";
-    cin >> year;
-    }
-    return year;
-}
-
 bool isLeapYear(int year){
     // cout << "\nC1: " << (year % 400) << "\nC2: " << (year % 4) << "\nC3: " << (year % 100);
     // cout << "\n"<<((year % 400 == 0) || ((year % 4==0) && (year % 100 != 0)));
     return ((year % 400 == 0) || ((year % 4==0) && (year % 100 != 0)));
+}
+
+int dayCheck(int day, int month, int year){
+    if (month == 2){
+        if (isLeapYear(year)){
+            while (!((1<=day)&&(day<=29))){
+                cout << "\nPlease enter a day between 1 and 29\nDay ";
+                cin >> day;
+            }
+        }
+        else {
+            while (!((1<=day)&&(day<=28))){
+                cout << "\nPlease enter a day between 1 and 28\nDay ";
+                cin >> day;
+        }
+        }
+    }
+    if ((month == 9) | (month == 4) | (month==6) | (month==11)){
+        while (!((1<=day)&&(day<=30))){
+        cout << "\nPlease enter a day between 1 and 30\nDay ";
+        cin >> day;
+        }
+    }
+    else  {
+        while (!((1<=day)&&(day<=31))){
+        cout << "\nPlease enter a day between 1 and 31\nDay ";
+        cin >> day;
+        }
+    }
+    return day;  
+}
+int monthCheck(int month){
+    while (!((1<=month)&&(month<=12))){
+            cout << "\nPlease enter a month between 1 and 12\nMonth: ";
+            cin >> month;
+     }
+    return month;
+}
+int yearCheck(int year){
+    while (!((0 <= year) && (year <= 9999))){
+    cout << "Please enter a year between 0 and 9999AD:\nYear ";
+    cin >> year;
+    }
+    return year;
 }
 
 int getCenturyValue(int year){
@@ -25,11 +61,49 @@ int getCenturyValue(int year){
 }
 
 int getYearValue(int year){
-    return getCenturyValue(year);
+   int a = (year - (year/100)*100);
+   int b = a/4;
+   return a+b; 
 }
 
-int getMonthValue(int month){
-    return 0;
+int getMonthValue(int month, int year){
+    switch(month){
+        case 1:
+            if (isLeapYear(year)){
+                return 6;
+            }
+            else
+                return 0;
+        case 2:
+            if (isLeapYear(year)){
+                return 2;
+            }
+            else
+                return 3;
+        case 3:
+            return 3;
+        case 4:
+            return 6;
+        case 5:
+            return 1;
+        case 6:
+            return 4;
+        case 7:
+            return 6;
+        case 8:
+            return 2;
+        case 9:
+            return 5;
+        case 10:
+            return 0;
+        case 11:
+            return 3;
+        case 12:
+            return 5;
+        default:
+            break;
+    }
+
 }
 
 string dayOfWeek(int dayValue){
@@ -59,24 +133,20 @@ string dayOfWeek(int dayValue){
 }
 
 int main(){
-    // cout << "Enter the date below\nMonth: ";
-    // cin >> month;
-    // cout << "\nDay: ";
-    // cin >> day;
-    // cout << "\nYear: ";
-    // cin >> year;
-
-    // cout << "The date is "<<month<<"/"<<day<<"/"<<year;
-
-    // cout << "\nEnter the day of week value: ";
-    // cin >> dayValue;
-    // cout << "\n"<< dayValue;
-    // cout << "\nThe day is a " << dayOfWeek(dayValue);
-
-    cout << "input: ";
+    cout << "Enter the date below\nYear: ";
     cin >> year;
-    // yearCheck(year);
-    cout << "\noutput: " << year;
+    year = yearCheck(year);
+    cout << "\nMonth: ";
+    cin >> month;
+    month = monthCheck(month);
+    cout << "\nDay: ";
+    cin >> day;
+    day = dayCheck(day,month,year);
 
+    // cout << "\nThe date is "<<month<<"/"<<day<<"/"<<year;
+
+    dayValue = (day + getMonthValue(month,year) + getYearValue(year) + getCenturyValue(year))%7;
+
+    cout << "\n\nThe day is a " << dayOfWeek(dayValue) << "\n\n";
 
 }
